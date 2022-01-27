6010c6bb4144d04c6208b48bff9a2673d10a7058
asorrow <asorrow@vols.utk.edu>
2022-01-26 19:15:55 -0600

    yeet

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,24 @@
 from flask import render_template
 from app.main import bp
+from app.main.Forms import EmailForm
+
 
 
 @bp.route('/')
 @bp.route('/home')
 def home():
+
+
     return render_template(
         'home.html',
         title='Home',
+
+        )
+@bp.route('/eatmyass')
+def eatmyass():
+    form = EmailForm()
+    return render_template(
+        'eatmyass.html',
+        title='Joe Byron',
+        form = form
     )
